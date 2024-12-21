5a837a20e9158a5795a287845eb9ac69f1b184a9
voidCounter <rohitroy7014@gmail.com>
2024-12-21 18:45:40 +0600

    Initial commit

---
+++ challenge_2/src/main/java/com/bitfest_preli/challenge_2/Challenge2Application.java
@@ -0,0 +1,13 @@
+package com.bitfest_preli.challenge_2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Challenge2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Challenge2Application.class, args);
+	}
+
+}
